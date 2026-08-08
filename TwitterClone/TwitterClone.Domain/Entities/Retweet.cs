268d9f8033bd1f34b1f2b0cf69a93041e98fd9c5
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-08 20:49:31 +0600

    Add Retweet Entity

---
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities;
+
+public class Retweet
+{
+    private Guid _userId;
+    private Guid _tweetId;
+    private DateTime _retweetAt;
+
+    public Guid UserId
+    {
+        get { return _userId; }
+    }
+
+    public Guid TweetId
+    {
+        get { return _tweetId; }
+    }
+
+    public DateTime RetweetAt
+    {
+        get { return _retweetAt; }
+    }
+}
