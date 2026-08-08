e6e788773c18c46217ab9b88da08be8fc51ba4a8
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-08 20:50:46 +0600

    Add Bookmark Entity

---
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities;
+
+public class Bookmark
+{
+    private Guid _userId;
+    private Guid _tweetId;
+    private DateTime _bookmarkedAt;
+
+    public Guid UserId
+    {
+        get { return _userId; }
+    }
+
+    public Guid TweetId
+    {
+        get { return _tweetId; }
+    }
+
+    public DateTime BookmarkedAt
+    {
+        get { return _bookmarkedAt; }
+    }
+}
