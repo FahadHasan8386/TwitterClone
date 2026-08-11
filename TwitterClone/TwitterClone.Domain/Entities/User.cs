55bfd69e8d21027835175120471107bdd4205518
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-11 11:43:51 +0600

    User Inherit base entity

--- TwitterClone/TwitterClone.Domain/Entities/User.cs
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -6,29 +6,32 @@ using System.Threading.Tasks;
 
 namespace TwitterClone.Domain.Entities;
 
-public class User
+public class User : BaseEntity
 {
-    private Guid _id;
     private string _firstName;
     private string _lastName;
     private string _email;
 
-    public User(string firstName, string lastName, string email)
+    public User() : base(Guid.NewGuid())
     {
-        _id = Guid.NewGuid();
-        _firstName = firstName;
-        _lastName = lastName;
-        _email = email;
-    }
-
-    public Guid Id
-    {
-        get { return _id; }
+       
     }
 
     public string FirstName
     {
         get { return _firstName; }
         set { _firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return _lastName; }
+        set { _lastName = value; }
+    }
+
+    public string Email
+    {
+        get { return _email; }
+        set { _email = value; }
     }
 }
