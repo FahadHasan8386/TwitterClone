4e3945c78dc8e6495f5002e925e0b0eeaadd1c9f
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-24 16:22:28 +0600

    Add Web Api controller like , retweet, messages

--- TwitterClone/TwitterClone.Api/Controllers/LikesController.cs
+++ TwitterClone/TwitterClone.Api/Controllers/LikesController.cs
@@ -1,12 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TwitterClone.Domain.Entities;
 
 namespace TwitterClone.Api.Controllers
 {
-    public class LikesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LikesController : ControllerBase
     {
-        public IActionResult Index()
+        // temporary in-memory storage
+        private static readonly List<Like> _likes = new();
+
+        // GET api/likes
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            return View();
+            return Ok(_likes);
+        }
+
+        // GET api/likes/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var like = _likes.FirstOrDefault(x => x.Id == id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(like);
+        }
+
+        // POST api/likes
+        [HttpPost]
+        public IActionResult Create(Like like)
+        {
+            _likes.Add(like);
+            return CreatedAtAction(nameof(GetById), new { id = like.Id }, like);
+        }
+
+        // PUT api/likes/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, Like updated)
+        {
+            var like = _likes.FirstOrDefault(x => x.Id == id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            like.UserId = updated.UserId;
+            like.TweetId = updated.TweetId;
+            return Ok(like);
+        }
+
+        // DELETE api/likes/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var like = _likes.FirstOrDefault(x => x.Id == id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            _likes.Remove(like);
+            return NoContent();
         }
     }
 }
--- TwitterClone/TwitterClone.Api/Controllers/MessagesController.cs
+++ TwitterClone/TwitterClone.Api/Controllers/MessagesController.cs
@@ -1,12 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TwitterClone.Domain.Entities;
 
 namespace TwitterClone.Api.Controllers
 {
-    public class MessagesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
     {
-        public IActionResult Index()
+        // temporary in-memory storage
+        private static readonly List<Message> _messages = new();
+
+        // GET api/messages
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            return View();
+            return Ok(_messages);
+        }
+
+        // GET api/messages/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var message = _messages.FirstOrDefault(x => x.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(message);
+        }
+
+        // POST api/messages
+        [HttpPost]
+        public IActionResult Create(Message message)
+        {
+            message.SentAt = DateTime.UtcNow; // set the time when the message is sent
+            _messages.Add(message);
+            return CreatedAtAction(nameof(GetById), new { id = message.Id }, message);
+        }
+
+        // PUT api/messages/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, Message updated)
+        {
+            var message = _messages.FirstOrDefault(x => x.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            message.SenderId = updated.SenderId;
+            message.ReceiverId = updated.ReceiverId;
+            message.Content = updated.Content;
+            message.IsRead = updated.IsRead;
+            return Ok(message);
+        }
+
+        // DELETE api/messages/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var message = _messages.FirstOrDefault(x => x.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            _messages.Remove(message);
+            return NoContent();
         }
     }
 }
