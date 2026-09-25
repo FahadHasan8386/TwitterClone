8d03fb8cffdfe3ffae769a9e1b1951af4f1c06ca
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-26 02:48:26 +0600

    update Controller for user and Tweet

--- TwitterClone/TwitterClone.Api/Controllers/TwittersController.cs
+++ TwitterClone/TwitterClone.Api/Controllers/TwittersController.cs
@@ -1,40 +1,146 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TwitterClone.Api.Dtos;
+using TwitterClone.Api.Repository;
 using TwitterClone.Domain.Entities;
 
-namespace TwitterClone.Api.Controllers
+namespace TwitterClone.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TweetsController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class TweetsController : ControllerBase
+    private readonly TweetRepository _tweetRepository;
+    private readonly UserRepository _userRepository;
+    public TweetsController(TweetRepository tweetRepository, UserRepository userRepository)
     {
-
-        private readonly IConfiguration _config;
-
-        public TweetsController(IConfiguration config)
-        {
-            _config = config;
-        }
-
-        [HttpGet]
-        public IActionResult GetTweets()
-        {
-
-            string? maxLength = _config["TweetSettings:MaxTweetLength"];
-
-            Tweet tweet = new Tweet(
-              Guid.NewGuid(),
-              Guid.NewGuid(),
-              "This is a new tweet",
-              DateTime.UtcNow,
-              Guid.NewGuid()
-            );
-
-
-            return Ok(new Dictionary<string, object>
-    {
-      {"maxLength", maxLength},
-      {"tweet", tweet },
-    });
-        }
+        _tweetRepository = tweetRepository;
+        _userRepository = userRepository;
     }
+
+
+    // GET /api/tweets?userId={userId}
+    [HttpGet]
+    public IActionResult GetTweets([FromQuery] Guid? userId)
+    {
+        List<Tweet> tweets;
+
+        if (userId.HasValue)
+        {
+            tweets = _tweetRepository.GetTweetsByUserId(userId.Value);
+        }
+        else
+        {
+            tweets = _tweetRepository.GetTweets();
+        }
+
+        var tweetDtos = tweets.Select(tweet => new TweetDto
+        {
+            Id = tweet.Id,
+            UserId = tweet.UserId,
+            Content = tweet.Content
+        });
+
+        return Ok(tweetDtos);
+    }
+
+    // GET /api/tweets/{id}
+    [HttpGet("{id}")]
+    public IActionResult GetTweetById([FromRoute] Guid id)
+    {
+        var tweet = _tweetRepository.GetTweetById(id);
+
+        if (tweet == null)
+        {
+            return NotFound();
+        }
+
+        var tweetDto = new TweetDto
+        {
+            Id = tweet.Id,
+            UserId = tweet.UserId,
+            Content = tweet.Content
+        };
+
+        return Ok(tweetDto);
+    }
+
+    // POST /api/tweets
+    [HttpPost]
+    public IActionResult CreateTweet([FromBody] CreateTweetRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Content))
+        {
+            return BadRequest("Tweet content is required.");
+        }
+
+        var user = _userRepository.GetUserById(request.UserId);
+
+        if (user == null)
+        {
+            return BadRequest("User does not exist.");
+        }
+
+        var tweet = new Tweet(request.Content)
+        {
+            UserId = request.UserId
+        };
+
+        _tweetRepository.AddTweet(tweet);
+
+        var tweetDto = new TweetDto
+        {
+            Id = tweet.Id,
+            UserId = tweet.UserId,
+            Content = tweet.Content
+        };
+
+        return Ok(tweetDto);
+    }
+
+    // PUT /api/tweets/{id}
+    [HttpPut("{id}")]
+    public IActionResult UpdateTweet([FromRoute] Guid id, [FromBody] UpdateTweetRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Content))
+        {
+            return BadRequest("Tweet content is required.");
+        }
+
+        var tweet = _tweetRepository.GetTweetById(id);
+
+        if (tweet == null)
+        {
+            return NotFound();
+        }
+
+        tweet.Content = request.Content;
+
+        _tweetRepository.UpdateTweet(tweet);
+
+        var tweetDto = new TweetDto
+        {
+            Id = tweet.Id,
+            UserId = tweet.UserId,
+            Content = tweet.Content
+        };
+
+        return Ok(tweetDto);
+    }
+
+    // DELETE /api/tweets/{id}
+    [HttpDelete("{id}")]
+    public IActionResult DeleteTweet([FromRoute] Guid id)
+    {
+        var tweet = _tweetRepository.GetTweetById(id);
+
+        if (tweet == null)
+        {
+            return NotFound();
+        }
+
+        var isDeleted = _tweetRepository.DeleteTweet(tweet);
+
+        return Ok(isDeleted);
+    }
+
 }
--- TwitterClone/TwitterClone.Api/Controllers/UsersController.cs
+++ TwitterClone/TwitterClone.Api/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TwitterClone.Api.Repository;
+using TwitterClone.Api.Dtos;
 using TwitterClone.Domain.Entities;
 
 namespace TwitterClone.Api.Controllers;
@@ -19,7 +22,127 @@ public class UsersController : ControllerBase
     public IActionResult GetUsers()
     {
         var users = _userRepository.GetUsers();
-        return Ok(users);
+
+        var user  = users.Select (users => new UserDto
+        {
+            Id = users.Id,
+            FirstName = users.FirstName,
+            LastName = users.LastName,
+            Email = users.Email
+        });
+        return Ok(user);
     }
 
+    [HttpPost]
+    [AllowAnonymous]
+    public IActionResult CreateUser([FromBody] CreateUserRequest request)
+    {
+        if(string.IsNullOrWhiteSpace(request.FirstName) || string.IsNullOrWhiteSpace(request.LastName) || string.IsNullOrWhiteSpace(request.Email))
+        {
+            return BadRequest("First name, last name, and email are required.");
+        }
+
+        var existingUser = _userRepository.GetUserByEmail(request.Email);
+
+        if (existingUser != null)
+        {
+            return BadRequest("A user with that email already exists.");
+        }
+
+        // Create the new user
+        var newUser = new User
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email
+        };
+
+        _userRepository.AddUser(newUser);
+
+        var userDto = new UserDto
+        {
+            Id = newUser.Id,
+            FirstName = newUser.FirstName,
+            LastName = newUser.LastName,
+            Email = newUser.Email
+        };
+        return Ok(userDto);
+    }
+
+    // /api/users/{id}
+    [HttpGet("{id}")]
+    public IActionResult GetUserById([FromRoute] Guid id)
+    {
+        var user = _userRepository.GetUserById(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userDto = new UserDto
+        {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email
+        };
+
+        return Ok(userDto);
+    }
+
+
+    // PUT /api/users/{id}
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(Guid id, [FromBody] UpdateUserRequest request)
+    {
+        var user = _userRepository.GetUserById(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var existingEmailUser = _userRepository.GetUserById(id);
+
+        user.FirstName = request.FirstName;
+        user.LastName = request.LastName;
+
+        _userRepository.UpdateUser(user);
+
+        var userDto = new UserDto
+        {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email
+        };
+
+        return Ok(userDto);
+    }
+
+
+    // PATCH /api/users/{id}/phoneNumber
+    [HttpPatch("{id}/phoneNumber")]
+    public IActionResult UpdateUserPhoneNumber([FromRoute] Guid id, [FromBody] string phoneNumber)
+    {
+        return Ok("hello");
+
+    }
+
+    // DELETE /api/users/{id}
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser([FromRoute] Guid id)
+    {
+        var user = _userRepository.GetUserById(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var isDeleted = _userRepository.DeleteUser(user);
+
+        return Ok(isDeleted);
+    }
 }
